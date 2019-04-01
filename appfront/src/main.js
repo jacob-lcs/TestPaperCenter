10a366bdf6660f9faa249259ce5c2671f54aa0e2
SoulCode <soul_code@126.com>
2019-04-01 10:46:06 +0800

    加入axios

--- appfront/src/main.js
+++ appfront/src/main.js
@@ -1,17 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import VueResource from 'vue-resource'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import axios from "axios";
+import VueResource from "vue-resource";
 
 Vue.use(VueResource);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.prototype.$axios = axios;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
