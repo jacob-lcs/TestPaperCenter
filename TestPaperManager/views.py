e88827b9af1736fbfafccfa19e2bcbb1986afd9b
lcs <lcs1998@vip.qq.com>
2019-05-07 15:38:12 +0800

    完成导入单选题逻辑并存储进入数据库
    1.检查用户有没有漏输
    2.将选项使用特殊字符分割
    3.将正确答案使用正确答案标记

--- TestPaperManager/views.py
+++ TestPaperManager/views.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
-from .models import User, Paper, QuestionDifficulty, QuestionTypes, KnowledgePoint
+from .models import User, Paper, QuestionDifficulty, QuestionTypes, KnowledgePoint, Grade, Subject, School, Paper, \
+    Question
 from django.contrib.auth.hashers import make_password, check_password
 from django.forms.models import model_to_dict
 
@@ -59,8 +60,89 @@ def query_types(request):
 # 查询知识点列表接口
 @require_http_methods(['GET'])
 def query_knowledgepoint(request):
+    # 递归查询
+    def search_children(a, b):
+        if len(a.children.all()) != 0:
+            ii = 0
+            for i in a.children.all():
+                if 'children' not in b.keys():
+                    b['children'] = []
+                b['children'].append({
+                    'value': i.name,
+                    'label': i.name
+                })
+                search_children(i, b['children'][ii])
+                ii += 1
+
     response = []
-    knowledgepoints = KnowledgePoint.objects.all()
+    knowledgepoints = KnowledgePoint.objects.filter(parent=None)
+    i = 0
     for knowledgepoint in knowledgepoints:
-        response.append(model_to_dict(knowledgepoint))
+        a = {
+            'value': knowledgepoint.name,
+            'label': knowledgepoint.name
+        }
+        response.append(a)
+        search_children(knowledgepoint, response[i])
+        i += 1
+    return JsonResponse(response, safe=False)
+
+
+# 查询年级列表接口
+@require_http_methods(["GET"])
+def query_grades(request):
+    response = []
+    grades = Grade.objects.all()
+    for grade in grades:
+        response.append(model_to_dict(grade))
+    return JsonResponse(response, safe=False)
+
+
+# 查询科目列表接口
+@require_http_methods(["GET"])
+def query_subjects(request):
+    response = []
+    subjects = Subject.objects.all()
+    for subject in subjects:
+        response.append(model_to_dict(subject))
+    return JsonResponse(response, safe=False)
+
+
+# 保存单选题接口
+@require_http_methods(['GET'])
+def save_single_topic_selection(request):
+    print(request.GET)
+    school_name = request.GET.get('school_name')
+    paper_name = request.GET.get('paper_name')
+    paper_year = request.GET.get('paper_year')
+    grade = request.GET.get('grade')
+    subject = request.GET.get('subject')
+    question_type = request.GET.get('question_type')
+    question_stem = request.GET.get('question_stem')
+    question_answer = request.GET.get('question_answer')
+    question_difficult = request.GET.get('question_difficult')
+    question_knowledgepoints = dict(request.GET).get('question_knowledgepoints[list][]')
+    sql_school_name = School.objects.filter(name=school_name)
+    if not sql_school_name:
+        School(name=school_name).save()
+    sql_school_name = School.objects.filter(name=school_name)
+    sql_subject_name = Subject.objects.filter(name=subject)
+    sql_grade = Grade.objects.filter(name=grade)
+    sql_paper_name = Paper.objects.filter(name=paper_name, year=paper_year, subject_name_id=sql_subject_name.first().id,
+                                          grade_id=sql_grade.first().id, school_name_id=sql_school_name.first().id)
+    if not sql_paper_name:
+        Paper(name=paper_name, year=paper_year, subject_name_id=sql_subject_name.first().id,
+              grade_id=sql_grade.first().id, school_name_id=sql_school_name.first().id).save()
+    sql_paper_name = Paper.objects.filter(name=paper_name)
+    sql_question_type = QuestionTypes.objects.filter(name=question_type)
+    sql_question_difficult = QuestionDifficulty.objects.filter(name=question_difficult)
+    paper = Question.objects.create(stem=question_stem, answer=question_answer, type_id=sql_question_type.first().id,
+                                    difficulty_id=sql_question_difficult.first().id,
+                                    paper_name_id=sql_paper_name.first().id)
+    knowledgepoints = []
+    for knowledgepoint in question_knowledgepoints:
+        knowledgepoints.append(KnowledgePoint.objects.filter(name=knowledgepoint).first().id)
+    for k in knowledgepoints:
+        paper.knowledge_point.add(k)
+    response = {"res": "success"}
     return JsonResponse(response, safe=False)
--- appfront/src/components/Test_Paper_Import.vue
+++ appfront/src/components/Test_Paper_Import.vue
@@ -9,39 +9,60 @@
         <span>添加试题</span>
       </p>
       <div>
+
+        <div style="margin-top: 15px; margin-bottom:15px; text-align: center">
+          <!--    试卷名称输入    -->
+          <Input size="large" v-model="paper_name" placeholder="请输入试卷名称" style="width: 300px;"/>
+          <!--    学校名称输入    -->
+          <Input size="large" v-model="school_name" placeholder="请输入学校名称" style="width: 300px;"/>
+        </div>
+        <!--  年级选择器  -->
+        <Select v-model="grade" style="width:200px" transfer placeholder="请选择年级">
+          <Option v-for="item in grade_list" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
+        <!--  科目选择器  -->
+        <Select v-model="paper_subject" style="width:200px" transfer placeholder="请选择科目">
+          <Option v-for="item in subject_list" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
         <!--  题型选择器  -->
-        <Select v-model="topic" style="width:200px" @on-change="topic_change" transfer="true" placeholder="请选择题目类型">
+        <Select v-model="topic" style="width:200px" @on-change="topic_change" transfer placeholder="请选择题目类型">
           <Option v-for="item in topicList" :value="item.value" :key="item.value">{{ item.label }}</Option>
         </Select>
         <!--  试题难度选择器  -->
-        <Select v-model="question_difficulty" style="width:200px" transfer="true" placeholder="请选择试题难度">
+        <Select v-model="question_difficulty" style="width:200px" transfer placeholder="请选择试题难度">
           <Option v-for="item in question_difficulty_list" :value="item.value" :key="item.value">{{ item.label }}
           </Option>
         </Select>
+        <!--    试卷年份选择器    -->
+        <DatePicker type="year" @on-change="select_year_change" format="yyyy" placeholder="请选择试卷年份" style="width: 200px"
+                    transfer></DatePicker>
         <br>
-        <!--    试卷信息输入    -->
-        <div style="margin-top: 15px; text-align: center">
-          <Input size="large" v-model="paper_name" placeholder="请输入试卷名称" style="width: 300px;"/>
-          <Input size="large" v-model="paper_year" placeholder="请输入试卷年份" style="width: 300px;margin-left:20px"/>
-          <Input size="large" v-model="paper_subject" placeholder="请输入科目" style="width: 300px;margin-left:20px"/>
 
-        </div>
         <!--  单选题  -->
         <div v-if="topic === '单选题'">
-          <!--     知识点标签     -->
-          <Tag v-for="item in knowledgepoint_list" :key="item" :name="item" closable @on-close="knowledgepoint_close">
-            标签{{ item + 1 }}
-            <Cascader :data="knowledge" :render-format="format"></Cascader>
-          </Tag>
+
           <!--     输入题干     -->
           <mavon-editor v-model="single_choice_content" ref=md @imgAdd="$imgAdd" @imgDel="$imgDel"
                         placeholder="请输入题干...." style="margin-top: 15px"></mavon-editor>
           <Divider>选项</Divider>
 
           <!--     选择题选项    -->
-          <div v-for="select_option in select_options">
-            <mavon-editor v-model="select_option.content" ref=md @imgAdd="$imgAdd" @imgDel="$imgDel"
-                          placeholder="请输入选项内容...." style="margin-top: 15px"></mavon-editor>
+          <RadioGroup v-model="question_answer_chosen" vertical>
+            <div v-for="select_option in select_options">
+              <Radio style="margin-top: 15px" size="large" :label="select_option.label"></Radio>
+              <mavon-editor v-model="select_option.content" ref=md @imgAdd="$imgAdd" @imgDel="$imgDel"
+                            placeholder="请输入选项内容...." style="margin-top: 5px; width: 70vw"></mavon-editor>
+            </div>
+          </RadioGroup>
+          <!--     知识点标签     -->
+          <div>
+            <Tag v-for="item in knowledgepoint_list" :key="item" :name="item" closable @on-close="knowledgepoint_close"
+                 style="margin-top: 15px">
+              {{ item }}
+            </Tag>
+            <Cascader :data="knowledge" @on-change="knowledge_point_change"
+                      transfer style="margin-top: 15px; width: 200px" change-on-select
+                      placeholder="请选择知识点标签"></Cascader>
           </div>
 
           <!--     添加和删除选项     -->
@@ -52,10 +73,9 @@
         </div>
       </div>
       <div slot="footer" style="text-align: center;">
-        <Button type="primary" icon="md-document">保存</Button>
-        <Button icon="md-folder">保存并关闭</Button>
-        <Button icon="md-flash">保存并新增</Button>
-        <Button type="error" icon="ios-close">关闭</Button>
+        <Button type="primary" icon="md-document" @click="save_and_close">保存并关闭</Button>
+        <Button icon="md-flash" @click="save_and_dont_close">保存并新增</Button>
+        <Button type="error" icon="ios-close" @click="close_import_paper">关闭</Button>
       </div>
     </Modal>
 
@@ -86,15 +106,16 @@
 </template>
 
 <script>
+  import {mavonEditor} from 'mavon-editor'
 
   export default {
     name: "Test_Paper_Import",
     data() {
       return {
-        importpaper_flag: false,  // 导入试题dialog显示标志位
         paper_name: '',  // 试卷名称
         paper_year: '',  // 试卷年份
-        paper_subject: '', // 试卷科目
+        paper_subject: '', // 已经选择的试卷科目
+        subject_list: [],  // 科目选择器的科目列表
         modal12: false,  // 导入试题对话框显示标志位
         topicList: [],  // 可供选择的题型
         topic: '',  //目前选择的题型
@@ -167,7 +188,7 @@
             }
           }
         ],
-        data1: [    // 数据
+        data1: [    // 题目数据
           {
             name: 'John Brown',
             age: 18,
@@ -197,17 +218,30 @@
         question_difficulty: '',  // 题目难度
         question_difficulty_list: [],  // 题目难度列表
         knowledgepoint_list: [],  // 题目知识点列表
-        knowledge:[],  // 级联选择器的知识点列表
+        knowledge: [],  // 级联选择器的知识点列表
+        grade_list: [],  // 年级列表
+        grade: '',  // 已经选择的年级
+        question_answer_chosen: '',  // 选择的正确答案
+        question_answer: '',  // 生成的正确答案
+        option_title: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],  // 选项标号
+        school_name: '',  // 输入的学校名称
+        import_success: false,  // 导入是否成功标志位
       }
     },
     methods: {
 
       // 添加选项
       add_select_option() {
-        var con = {
-          content: ''
-        };
-        this.select_options.push(con)
+        if (this.select_options.length === 24) {
+          this.$Message.warning('最多只能添加24个选项哦');
+        } else {
+          let label = this.option_title[this.select_options.length];
+          let con = {
+            content: '',
+            label: label
+          };
+          this.select_options.push(con)
+        }
       },
 
       // 删除选项
@@ -215,10 +249,7 @@
         this.select_options.pop()
       },
 
-      add_paper() {
-        this.importpaper_flag = true
-      }
-      ,
+
       // 导入试题dialog点击确定触发的事件
       importpaper_ok() {
         let that = this;
@@ -242,30 +273,21 @@
             }
           })
         }
-
-      }
-      ,
-      // 导入试题dialog点击取消触发的事件
-      importpaper_cancel() {
-        this.$Message.info('试卷未创建');
-      }
-      ,
+      },
 
       // 题型发生变化
       topic_change() {
         console.log(this.topic)
-      }
-      ,
+      },
 
       // 图片上传
       $imgAdd(pos, $file) {
-        let that = this
         let file = Bmob.File($file.name, $file);
         file.save().then(res => {
           console.log(res.length);
           console.log(res);
           // 将图片链接改为图片地址
-          that.$refs.md.$img2Url(pos, res[0]['url']);
+          this.$refs.md.$img2Url(pos, res[0]['url']);
         })
       },
 
@@ -273,14 +295,178 @@
       $imgDel(pos, $file) {
 
       },
-
-      knowledgepoint_close() {
+      // 关闭知识点标签触发的事件
+      knowledgepoint_close(event, name) {
         const index = this.knowledgepoint_list.indexOf(name);
         this.knowledgepoint_list.splice(index, 1);
+      },
+
+      // 知识点级联选择变化触发的事件
+      knowledge_point_change(value, selectedData) {
+        console.log("触发级联选择器change事件");
+        if (this.knowledgepoint_list.indexOf(value[value.length - 1]) === -1) {
+          this.knowledgepoint_list.push(value[value.length - 1]);
+        }
+      },
+
+      // 关闭导入试题对话框触发的事件
+      close_import_paper() {
+        this.modal12 = false
+      },
+
+      // 生成答案
+      generate_answer() {
+        let that = this;
+        for (var i = 0; i < that.select_options.length; i++) {
+          if (that.question_answer_chosen === that.select_options[i]['label']) {
+            that.question_answer = that.question_answer.concat("lcshchzlm" + that.select_options[i]['label'] + ".%$@#$%" + that.select_options[i]['content'])
+          } else {
+            that.question_answer = that.question_answer.concat("lcshchzlm" + that.select_options[i]['label'] + "." + that.select_options[i]['content'])
+          }
+        }
+        console.log("生成的答案为：", that.question_answer)
+      },
+
+      // 导入试题
+      import_question() {
+        console.log("import_question()");
+        let that = this;
+        let kk = {'list': that.knowledgepoint_list};
+        $.ajax({
+          url: that.$site + "api/save_single_topic_selection",
+          dataType: "json",
+          data: {
+            school_name: that.school_name,
+            paper_name: that.paper_name,
+            paper_year: that.paper_year,
+            grade: that.grade,
+            subject: that.paper_subject,
+            question_type: that.topic,
+            question_stem: that.single_choice_content,
+            question_answer: that.question_answer,
+            question_difficult: that.question_difficulty,
+            question_knowledgepoints: kk
+          },
+          success: function (data) {
+            console.log(data);
+            if (data['res'] === "success") {
+              that.$Message.info("导入成功！");
+              that.successfully_import_clear();
+            } else {
+              that.$Message.warning("请检查网络！")
+            }
+          }
+        });
+      },
+
+
+      // 检查试卷基本信息有无没有输入的数据
+      check_paper_content() {
+        let that = this;
+        if (that.knowledgepoint_list.length === 0) {
+          that.$Message.warning("请选择知识点");
+          return false;
+        } else if (that.paper_year === '') {
+          that.$Message.warning("请输入年份");
+          return false;
+        } else if (that.paper_name === '') {
+          that.$Message.warning("请输入试卷名称");
+          return false;
+        } else if (that.paper_subject === '') {
+          that.$Message.warning("请选择试卷科目");
+          return false;
+        } else if (that.topic === '') {
+          that.$Message.warning("请选择题目类型");
+          return false;
+        } else if (that.grade === '') {
+          that.$Message.warning("请输入年级");
+          return false;
+        } else if (that.school_name === '') {
+          that.$Message.warning("请输入学校名称");
+          return false;
+        } else if (that.question_difficulty === '') {
+          that.$Message.warning("请选择题目难度");
+          return false;
+        } else {
+          return true;
+        }
+      },
+
+
+      // 检查单选题输入的内容
+      check_signal_select_content() {
+        let that = this;
+        if (that.single_choice_content === '') {
+          that.$Message.warning("请输入题目内容");
+          return false;
+        } else if (that.question_answer_chosen === '') {
+          that.$Message.warning("请选择正确答案");
+          return false;
+        } else if (!that.check_select_options_has_null()) {
+          that.$Message.warning("请输入选项内容");
+          return false;
+        } else {
+          return true;
+        }
+      },
+
+
+      // 判断选择题选项是否有空
+      check_select_options_has_null(){
+        let that = this;
+        for(var i=0; i<that.select_options.length; i++){
+          if(that.select_options[i]['content'] === ''){
+            return false;
+          }
+        }
+        return true;
+      },
+
+      // 保存并关闭对话框
+      save_and_close() {
+        console.log("导入试题中......");
+        let that = this;
+        if (that.check_paper_content()) {
+          console.log("进入第二级");
+          if (that.topic === "单选题") {
+            if (that.check_signal_select_content()) {
+              that.generate_answer();
+              that.import_question();
+              that.modal12 = false;
+            }
+          }
+        }
+      },
+
+
+      // 导入试题成功则清空数据
+      successfully_import_clear() {
+        let that = this;
+        console.log("清空数据....");
+        that.question_answer_chosen = '';
+        that.single_choice_content = '';
+        that.select_options = [];
+        that.knowledgepoint_list = [];
+        that.question_answer_chosen = '';
+        that.question_answer = '';
+
+      },
+
+
+      // 保存并导入下一个题目
+      save_and_dont_close() {
+        let that = this;
+        that.generate_answer();
+        that.import_question();
+      },
+
+
+      // 日期选择器改变
+      select_year_change(year) {
+        this.paper_year = year;
+        console.log(this.paper_year)
       }
-
     },
-
 
     mounted() {
       let that = this;
@@ -297,7 +483,7 @@
         dataType: "json",
         data: {},
         success: function (data) {
-          console.log(data)
+          console.log(data);
           for (var i = 0; i < data.length; i++) {
             that.question_difficulty_list.push({
               value: data[i]['name'],
@@ -316,6 +502,52 @@
           console.log(data);
           for (var i = 0; i < data.length; i++) {
             that.topicList.push({
+              value: data[i]['name'],
+              label: data[i]['name']
+            })
+          }
+        }
+      })
+
+      //  获取知识点列表
+      $.ajax({
+        url: that.$site + "api/query_knowledgepoint",
+        dataType: "json",
+        data: {},
+        success: function (data) {
+          console.log(data);
+          for (var i = 0; i < data.length; i++) {
+            that.knowledge.push(data[i])
+          }
+        }
+      });
+
+      //  获取科目列表
+      $.ajax({
+        url: that.$site + "api/query_subjects",
+        dataType: "json",
+        data: {},
+        success: function (data) {
+          console.log(data);
+          for (var i = 0; i < data.length; i++) {
+            that.subject_list.push({
+              value: data[i]['name'],
+              label: data[i]['name']
+            })
+          }
+        }
+      });
+
+
+      //  获取年级列表
+      $.ajax({
+        url: that.$site + "api/query_grades",
+        dataType: "json",
+        data: {},
+        success: function (data) {
+          console.log(data);
+          for (var i = 0; i < data.length; i++) {
+            that.grade_list.push({
               value: data[i]['name'],
               label: data[i]['name']
             })
