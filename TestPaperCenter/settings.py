3cee0799a3c8f9aa7bfa3aa2a288d2fc2a8e4b72
SoulCode <soul_code@126.com>
2019-05-17 11:19:03 +0800

    CORS配置

--- TestPaperCenter/settings.py
+++ TestPaperCenter/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'TestPaperManager.middlewares.Cros'
 ]
 CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'TestPaperCenter.urls'
